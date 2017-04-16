83698ca8f1151f016ffd4a3fb0808e20dd7d7b02
邱永臣 <qiuych3@mail2.sysu.edu.cn>
2017-04-17 02:26:58 +0800

    init

---
+++ src/main/java/com/iloveqyc/service/Impl/ILoveYouServiceImpl.java
@@ -0,0 +1,17 @@
+package com.iloveqyc.service.Impl;
+
+import com.iloveqyc.sample.api.ILoveYouService;
+import org.springframework.stereotype.Service;
+
+/**
+ * User: qiuyongchen Nicolas.David
+ * Date: 2017/4/10
+ * Time: 下午11:38
+ * Usage: xxx
+ */
+@Service("iLoveYouService")
+public class ILoveYouServiceImpl implements ILoveYouService {
+    public String iLoveYou(String yourName, String yourLoverName) {
+        return "这是" + yourName + "给他女朋友" + yourLoverName + "写的接口回复哦";
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: qiuyongchen
+  Date: 2017/4/17
+  Time: 上午2:13
+  To change this template use File | Settings | File Templates.
+--%>
+
+<html>
+  <head>
+    <title>Hi</title>
+  </head>
+  <body>
+  hell world!
+  </body>
+</html>
